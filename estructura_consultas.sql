b5f53306e50bc46d5096838b56d0979b92f151eb
Juan Ponce <107893374+juandevl@users.noreply.github.com>
2024-01-10 21:05:21 -0300

    Update estructura_consultas.sql

--- estructura_consultas.sql
+++ estructura_consultas.sql
@@ -3,20 +3,20 @@
 
 --SELECT nombre_columna FROM nombre_tabla WHERE (filtro) nombre_columna = valor_a_filtrar
 
---SELECT id FROM ESTILOS where Descripcion = 'Rock';
---SELECT * FROM DISCOS;
---SELECT * FROM ESTILOS;
---SELECT * FROM TIPOSEDICION;
+SELECT id FROM ESTILOS where Descripcion = 'Rock';
+SELECT * FROM DISCOS;
+SELECT * FROM ESTILOS;
+SELECT * FROM TIPOSEDICION;
 
 --INSERTAR DATOS
 -- INSERT INTO nombre_tabla (opcional=> nombre_columna1, nombre_columna2, ...) VALUES (valor_columna1, valor_columna2, ...)
 
---INSERT INTO DISCOS 
---VALUES ('Amapola del 66', 
---		SMALLDATETIMEFROMPARTS(2010,10,6,12,45), 12, 
---		'amapoladel66.jpg', 
---		(SELECT Id FROM ESTILOS WHERE Descripcion = 'Rock'),
---		(SELECT Id FROM TIPOSEDICION WHERE Descripcion = 'CD'));
+INSERT INTO DISCOS 
+VALUES ('Amapola del 66', 
+		SMALLDATETIMEFROMPARTS(2010,10,6,12,45), 12, 
+		'amapoladel66.jpg', 
+		(SELECT Id FROM ESTILOS WHERE Descripcion = 'Rock'),
+		(SELECT Id FROM TIPOSEDICION WHERE Descripcion = 'CD'));
 
 --############ Otra funcion para obtener la fecha es: getdate()
 
@@ -26,9 +26,9 @@
 --MODIFICAR REGISTRO
 
 --Sentencia UPDATE nombre_tabla SET nombre_columna = valor_a_cargar WHERE (filtro para no modificar toda la tabla) nombre_identificador = valor_buscado
---UPDATE TIPOSEDICION SET Descripcion = 'Cassette' WHERE Id = 3;
+UPDATE TIPOSEDICION SET Descripcion = 'Cassette' WHERE Id = 3;
 
 --ELIMINAR DE LA TABLA
 
---DELETE FROM ESTILOS WHERE Descripcion = 'Folklore';
+DELETE FROM ESTILOS WHERE Descripcion = 'Folklore';
 
